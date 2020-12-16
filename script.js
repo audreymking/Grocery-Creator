6371e68177e982e4c67dcbdbb23c8ba37485de7b
mcheering <62622009+mcheering@users.noreply.github.com>
2020-12-15 21:21:26 -0600

    adding files for project

---
+++ script.js
@@ -0,0 +1,21 @@
+// //Edamam API
+//       //c830e7a5 edamam application key 
+//             //1583ebbcc0b71b443409e71ce9aeb0c0 edamam application key used to authenticate requests 
+
+//             //example 
+//             const settings = {
+//                   "async": true,
+//                   "crossDomain": true,
+//                   "url": "https://edamam-recipe-search.p.rapidapi.com/search?q=chicken",
+//                   "method": "GET",
+//                   "headers": {
+//                         "x-rapidapi-key": "bf44b73a88msh48ab9ef42ebfc0fp1dc138jsnca698990928b",
+//                         "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com"
+//                   }
+//             };
+
+//             $.ajax(settings).done(function (response) {
+//                   console.log(response);
+//             });
+
+
