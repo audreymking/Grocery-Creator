e062c7ccfabb18d79ac72168702f7aeb890659e6
Matthew Heering <matthew.c.heering01@gmail.com>
2020-12-17 18:15:58 -0600

    fixed styling on hndex.html and recipeBook.html

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+$("document").ready(function () {
+      const searchTerm = $("#search").val()
+      const video = $("#youtTubeVideo")
+      const recipeTitle = $("#recipeTitle")
+      const ingredients = $("#ingredients")
+      const saveButton = $("#saveButton")
+      const clearButton = $("#clearButton")
+      const recipesButton = $("#recipesButton")
+
+
+      searchTerm.keypress(function (event) {
+            if (event.which === 13) {
+                  event.preventDefault()
+                  const settings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": "https://edamam-recipe-search.p.rapidapi.com/search?q" + searchTerm,
+                        "url": "https://api.edamam.com/search?q=chicken&app_key=$" + headers[0] + "&from=0&to=3"
+                        "method": "GET",
+                        "headers": {
+                              "x-rapidapi-key": "bf44b73a88msh48ab9ef42ebfc0fp1dc138jsnca698990928b",
+                              "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com"
+                        }
+                  };
+
+                  $.ajax(settings).then(function (response) {
+                        console.log(response);
+                  });
+            }
+      });
+})
+
+
 // //Edamam API
 //       //c830e7a5 edamam application key 
 //             //1583ebbcc0b71b443409e71ce9aeb0c0 edamam application key used to authenticate requests 
@@ -19,3 +52,5 @@
 //             });
 
 
+// Youtube API
+//AIzaSyCpinsFzK8YgE-y0pDgj1f7ZuGlsVdjrkI key
